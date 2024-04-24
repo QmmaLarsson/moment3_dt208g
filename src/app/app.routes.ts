17c8b0a23cf5d03a36bd947a0085010f0caa3ad1
Emma Larsson <emma940112@gmail.com>
2024-04-24 11:09:23 +0200

    Startat ett Angularprojekt, skapat tre undersidor samt huvudmeny och footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConverterComponent } from './converter/converter.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "about", component: AboutComponent},
+    {path: "converter", component: ConverterComponent},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "404", component: NotFoundComponent},
+    {path: "**", component: NotFoundComponent}
+];
